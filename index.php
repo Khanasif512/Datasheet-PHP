386fac1be8754b28be1fef6dbcbc818984aadc5d
Khanasif512 <asifkhan108526@gmail.com>
2024-04-18 22:04:24 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Login Registration System</title>
+</head>
+<body>
+    <h3 class="display-4">Checking Bootstrap</h3>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container">
+        <a href="index.php" class="navbar navbar-brand"><h3>L&R System</h3></a>
+        <ul class="navbar-nav m-auto">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+            <a href="#">Home</a>
+            <a href="#">Home</a>
+            <a href="#">Home</a>
+
+          </li>
+        </ul>
+      </div>
+</body>
+</html>
